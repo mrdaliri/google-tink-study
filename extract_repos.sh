ed26f05ec70e6e3f72db71f1bd1b14c35c29a343
Mohammad-Reza <rmdaliri@gmail.com>
2019-12-01 13:16:16 -0700

    Fix depth parameter for `find` in extract_repos.sh

--- extract_repos.sh
+++ extract_repos.sh
@@ -4,7 +4,7 @@ extract_repo() {
   zip_file=$1
   output_path=$2;
   unzip -qq "$1" -d "$2_temp";
-  created_dir=`find "$2_temp" -type d -depth 1`
+  created_dir=`find "$2_temp" -maxdepth 1 -mindepth 1 -type d`
   mv -f "$created_dir" "$2" && rm -rf "$2_temp";
 }
 
