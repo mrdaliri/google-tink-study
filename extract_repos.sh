4905699e1d5c7dfdbfa561895c5e75e2af8f3c02
Mohammad-Reza <rmdaliri@gmail.com>
2019-12-01 13:10:09 -0700

    Add extract_repos.sh

---
+++ extract_repos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+repos_folder=$1
+extract_repo() {
+  zip_file=$1
+  output_path=$2;
+  unzip -qq "$1" -d "$2_temp";
+  created_dir=`find "$2_temp" -type d -depth 1`
+  mv -f "$created_dir" "$2" && rm -rf "$2_temp";
+}
+
+for f in `find "${repos_folder}" -type f -name "*.zip"`;
+  do extract_repo $f "${f/.zip/}";
+done
